57f94a308dd715beaa35911f1114d9e90050640b
noevazz <jonovagu@gmail.com>
2025-03-16 21:11:40 -0600

    added game section for numbers 11 to 19 and hidden trickysillables.html

--- logicforcardsgame.js
+++ logicforcardsgame.js
@@ -3,7 +3,11 @@ function getRandomItem(array) {
 }
 
 function shuffle(array) {
-  return array.sort(() => Math.random() - 0.5);
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 let position = 0;
