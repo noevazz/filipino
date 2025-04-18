5cb9fad61359df8b340d9b25f3de20fe853ede91
noevazz <jonovagu@gmail.com>
2025-04-18 15:18:31 -0600

    removed title from images in games/answers sections

--- partials/games/js/answers.js
+++ partials/games/js/answers.js
@@ -30,7 +30,6 @@ window.wordsNamespace = {
         img.className = "mx-auto d-block jiggle";
         img.style.filter = "drop-shadow(3px 2.5px 4px #000A)";
         img.style.width = "50%";
-        img.title = `${item.filipino}/${item.english}`;
         img.alt = `${item.filipino}/${item.english}`;
         div.appendChild(img);
       }
