44597ea5b4529ece9632a7058a1ce993cd313048
noevazz <jonovagu@gmail.com>
2025-05-09 15:27:57 -0600

    added si ang section to misc

--- js/router.js
+++ js/router.js
@@ -789,6 +789,12 @@ window.routerNamespace = {
         accordionName: "miscellaneous",
         links: [
           {
+            key: "#/misc-si-ang",
+            path: "partials/misc/siang.html",
+            navbarName: "Si and Ang",
+            icon: "bi-book",
+          },
+          {
             key: "#/misc-family",
             path: "partials/misc/family.html",
             navbarName: "Family",
